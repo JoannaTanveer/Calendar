9e6ea50fc4c4367364743ff22da2492b02b55541
JoannaTanveer <joanna.tanveer@gmail.com>
2020-07-29 11:05:28 -0700

    local storage complete

--- script.js
+++ script.js
@@ -1,102 +1,71 @@
-var currentDay = $('#currentDay');
+var currentDay = $("#currentDay");
+var day=moment().format('MMMM Do YYYY')
 var scheduleText = $('textarea');
 var timeBlock = $('.time-block');
-var timeNow = moment().format('H');
-var todoList = [];
+var timeNow = moment().format('HH');
+console.log(timeNow)
 
-//Get items to save into local storage
 
-// function scheduleObjectsFn () {
-//     // For each row of time, get data-hour and name it as that
-//     timeBlock.each(function (){ 
-//     var thisTimeBlock = $(this);
-//     var thisHourRow = parse(thisTimeBlock.attr('data-hour'));
 
-    // For each scheduled thing put in text area add to an object with time being par tof the key value pair
-    var scheduleObject = {
-        hour: thisHourRow,
-        text: '',
-    }
+document.addEventListener('DOMContentLoaded', function() {
 
-    // Push this object into another bucket, this is the todoList
-    todoList.push(scheduleObject);
-// });
+
+
+
 
 // Save into local storage
-function storeMeetings () {
-localStorage.setItem('todoList', JSON.stringify(todoList));
-console.log(todoList)
+function saveinLocalStorage (key, value) {
+localStorage.setItem(key, JSON.stringify(value));
+console.log(key, value)
 }
 
-$(???).on('click', function(event) {
+$('.saveBtn').on('click', function(event) {
     event.preventDefault();
     
-    // Grabs input from textarea
+    // Grabs hour of 'this' save button
+    var hour =$(this).parent().attr('data-hour')
+    console.log(hour)
+    var meeting = $(this).siblings('.description').val().trim();
+    console.log(meeting);
+    saveinLocalStorage(hour, meeting)
     
-    
-    var meeting = $ (timeBlock).val().trim();
-    // Adding the meeting from textarea to our array 
-
-    if (meeting=== '') {
-        return;
-    }
-
-    todoList.push(scheduleObject);
-
-    i need to render something somewhere
 });
 
 
+$('#9am .description').val(JSON.parse(localStorage.getItem('9')));
+$('#10am .description').val(JSON.parse(localStorage.getItem('10')));
+$('#11am .description').val(JSON.parse(localStorage.getItem('11')));
+$('#12 .description').val(JSON.parse(localStorage.getItem('12')));
+$('#13 .description').val(JSON.parse(localStorage.getItem('13')));
+$('#14 .description').val(JSON.parse(localStorage.getItem('14')));
+$('#15 .description').val(JSON.parse(localStorage.getItem('15')));
+$('#16 .description').val(JSON.parse(localStorage.getItem('16')));
+$('#17 .description').val(JSON.parse(localStorage.getItem('17')));
+$('#18 .description').val(JSON.parse(localStorage.getItem('18')));
+
 // Change colors of rows based on current time
+})
+
 
 function colorChange(){
+    timeBlock.each(function(){
+    var thisHour = $(this)
 
-    if (thisHourRow == timeNow) {
+    var timeHour = parseInt(thisHour.attr("data-hour"));
+    console.log(timeHour)
+    if (timeHour == timeNow) {
 
         timeBlock.addClass('present');
-    } else if (thisHourRow < timeNow) {
+    } else if (timeHour < timeNow) {
         timeBlock.addClass('past');
-    } else if (thisHourRow > timeNow) {
+    } else if (timeHour> timeNow) {
         timeBlock.addClass('future')
     }
+}) 
 
 
 
+colorChange()
 
-
-
+currentDay.text(day);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-currentDay.innerHTML = moment().format('MMMM Do YYYY');
-
-
